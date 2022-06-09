bf3f85b778f66719811b0b0c1ad7e4d95ff9a1c6
Osvak <61042986+Osvak@users.noreply.github.com>
2022-06-09 19:43:16 +0200

    Intro done
    Uwu
    
    Co-Authored-By: Ignasi Pardo Carbó <60847464+kuronoascarlet@users.noreply.github.com>

---
+++ VackRooms/Assets/Scripts/Rooms/IntroScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.transform.tag == "Controller")
+        {
+            Debug.Log("KFADKLSHFADSHKLFADSHLAHSFKLADHSKLFHAKLSFHADKLSHAFSKLF");
+            player.transform.position = new Vector3(23, 0, 0.85f);
+        }  
+    }
+}
