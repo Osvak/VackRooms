9f142d226044686ee7bd5b67923f1fff2f0a2b14
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2022-06-10 11:51:04 +0200

    Audio volume mixing done

--- VackRooms/Assets/Scripts/Rooms/IntroScript.cs
+++ VackRooms/Assets/Scripts/Rooms/IntroScript.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class IntroScript : MonoBehaviour
 {
     public GameObject player;
+    public List<AudioMixerGroup> mixers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mixers[0].audioMixer.SetFloat("Volume F", -80.0f);
     }
 
     // Update is called once per frame
@@ -24,6 +27,8 @@ public class IntroScript : MonoBehaviour
             Debug.Log("KFADKLSHFADSHKLFADSHLAHSFKLADHSKLFHAKLSFHADKLSHAFSKLF");
             player.transform.position = new Vector3(23, 0, 0.85f);
             GameObject.Find("Intro").GetComponent<AudioSource>().enabled = false;
+            mixers[0].audioMixer.SetFloat("Volume F", 0.0f);
+            mixers[1].audioMixer.SetFloat("Volume I", -40.0f);
         }  
     }
 }
